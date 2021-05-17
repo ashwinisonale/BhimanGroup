70c2f33531d89fadc4e30ce5c147e2a8abdd2916
suchetasawant <suchetasawant1o91@gnail.com>
2021-05-17 13:37:35 +0530

    first commit

--- src/test/java/com/BhimanGroup/tests/MasterTest.java
+++ src/test/java/com/BhimanGroup/tests/MasterTest.java
@@ -21,7 +21,7 @@ public class MasterTest extends MainBase{
 		
 		String user = Constants.prop.getProperty("username");
 		String pass = Constants.prop.getProperty("password");
-		loginFlow(user, pass);
+		LoginFlow(user, pass);
 		
 		master = PageFactory.initElements(Constants.driver, MasterPage.class);
 		
