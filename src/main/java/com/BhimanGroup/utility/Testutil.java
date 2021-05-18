e625ad3b8b36b297ee84a5d91e13ed999a6cec01
a.m.sonale1336@gmail.com <a.m.sonale1336@gmail.com>
2021-05-18 15:33:34 +0530

    hi

--- src/main/java/com/BhimanGroup/utility/Testutil.java
+++ src/main/java/com/BhimanGroup/utility/Testutil.java
@@ -17,9 +17,7 @@ public class Testutil {
 	public static String excelFilePath = "src\\main\\java\\com\\"
 			+ "BhimanaGroup\\testData\\BhimanGroup.xlsx";
 	public static String sheetName = "login";
-	
-	//public static String sheetName = "masterTabFunction";
-	
+
 	public static Object[][] getValidInvalidTestDataFromExcel() {
 		Object[][] data = null;
 		try {
