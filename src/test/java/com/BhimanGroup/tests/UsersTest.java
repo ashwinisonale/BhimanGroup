b05f6ad25b0896f50df84cba272015f619fd969a
a.m.sonale1336@gmail.com <a.m.sonale1336@gmail.com>
2021-05-20 12:12:03 +0530

    User Page done

--- src/test/java/com/BhimanGroup/tests/UsersTest.java
+++ src/test/java/com/BhimanGroup/tests/UsersTest.java
@@ -1,5 +1,7 @@
 package com.BhimanGroup.tests;
 
+import java.io.IOException;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -9,31 +11,40 @@ import com.BhimanGroup.Pages.MasterPage;
 import com.BhimanGroup.Pages.UsersPage;
 import com.BhimanGroup.mainBase.Constants;
 import com.BhimanGroup.mainBase.MainBase;
+import com.BhimanGroup.utility.screenShot;
+
 
 public class UsersTest extends MainBase {
 
 
 	UsersPage userPage;	
-	//MasterTest test=new MasterTest();
 	MasterPage masterPage=new MasterPage(); 
-	
+	screenShot screen=new screenShot();
+
+
 	@BeforeClass	
 	public void BrowserSetUp() {
-		
+
 		String user = Constants.prop.getProperty("username");
 		String pass = Constants.prop.getProperty("password");
 		loginFlow(user, pass);
 		userPage=PageFactory.initElements(Constants.driver, UsersPage.class);
-		
-		
-		
+
+
+
 		masterPage.clickOnMasterTab();
 		masterPage.clickOnUserTab();
+		
 	}
-	@Test
+	@Test(priority=1,enabled=false)
 	public void AddUserButtonTest() {
 		implicitWait();
 		userPage.AddUserButtonTab();
+	}	
+
+	@Test(priority=2,enabled=false)
+	public void usersFormDetailsPageTest()
+	{
 		userPage.userPagePersonalDetails();
 		userPage.usersBankDetails();
 		implicitWait();
@@ -41,13 +52,21 @@ public class UsersTest extends MainBase {
 		implicitWait();
 		userPage.usersFatherDetails();
 		userPage.usersOtherDetails();
+		userPage.userSubmitTab();
 	}
 
-
-     @AfterClass
-     public void closeWindow() {
-	//tearDown();
+ @Test(priority=3)
+  public void searchButtonOnUsersTest(){
+	//implicitWait();
+	userPage.searchButtonOnUsersPage("ashwini");
+	screen.takeScreenShot();
+	
 }
+
+	@AfterClass
+	public void closeWindow() {
+		//tearDown();
+	}
 
 
 }
