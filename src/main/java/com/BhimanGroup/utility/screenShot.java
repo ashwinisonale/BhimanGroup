facf026d115e7b2242246238ad38afccbb28dc17
a.m.sonale1336@gmail.com <a.m.sonale1336@gmail.com>
2021-05-20 12:28:30 +0530

    user page screenshot taken

---
+++ src/main/java/com/BhimanGroup/utility/screenShot.java
@@ -0,0 +1,43 @@
+package com.BhimanGroup.utility;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import com.BhimanGroup.mainBase.Constants;
+
+public class screenShot {
+
+	
+	public static void takeScreenShot(){
+		
+		File file=((TakesScreenshot)Constants.driver).getScreenshotAs(OutputType.FILE);
+		try {
+			
+			FileUtils.copyFile(file, new File("C:\\Users\\Lenovo\\eclipse-projects\\com.BhimanGroup"
+					+ ""
+					+ "\\src\\main\\resources\\screenShot\\"+getCurrentDateOrTime()+".png"));
+		
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+		
+	public  static String getCurrentDateOrTime() {
+		
+		DateFormat formate=new SimpleDateFormat("mm_dd_yyyy_hh_mm_ss");
+		Date currentdate=new Date();
+		return formate.format(currentdate);
+	}
+	
+}
+	
